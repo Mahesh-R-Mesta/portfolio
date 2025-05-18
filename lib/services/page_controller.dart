1578c724dfdeff9ca71c26ec78d997709cd16f24
mahesh <mahesh.mesta@codematrix.org>
2025-05-18 22:09:27 +0530

    initial page

--- lib/services/page_controller.dart
+++ lib/services/page_controller.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ViewPageController extends PageController {
-  ViewPageController() : super(initialPage: 3);
+  ViewPageController() : super(initialPage: 0);
   Function(int)? onChange;
   navigateTo(int page) async {
     await animateToPage(page, duration: const Duration(milliseconds: 700), curve: Curves.decelerate);
