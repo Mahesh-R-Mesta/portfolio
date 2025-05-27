f90ee9d202f1d429e0184ca776576db5013ee3e0
Mahesh-R-Mesta <Mahesh-R-Mesta@users.noreply.github.com>
2025-05-27 07:57:56 +0000

    Commit the artifacts.

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -105,7 +105,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "3743044c2ceca022b033fa71737713fd",
+"flutter_bootstrap.js": "9f590012a10917537350630c86cf0b93",
 "favicon.png": "357a0e21c8d3facf5ed4f9b630c709b4",
 "index.html": "757f447a20d4b1295d773b2e4128204a",
 "/": "757f447a20d4b1295d773b2e4128204a",
