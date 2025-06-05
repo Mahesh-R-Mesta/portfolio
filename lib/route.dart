b26a510a8b22b3b5c2c5d99c6a2eae1e94260077
mahesh <mahesh.mesta@codematrix.org>
2025-06-05 22:53:49 +0530

    differ load and html loader

--- lib/route.dart
+++ lib/route.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:myportfolio/ui/about.dart';
-import 'package:myportfolio/ui/experience.dart';
-import 'package:myportfolio/ui/home.dart';
+
+import 'package:myportfolio/ui/splash_screen.dart';
 
 class RouteService {
   static const String INITIAL = "/home";
+  static const String LOADING = "/loading";
 
   static Route<dynamic>? generateRoute(RouteSettings setting) {
     switch (setting.name) {
-      case INITIAL:
+      case LOADING:
         {
           return PageRouteBuilder(pageBuilder: (ctx, animation1, animation2) {
-            return HomePage();
+            return LoadingScreen();
           });
         }
-      // case AboutPage.name:
-      //   {
-      //     return PageRouteBuilder(
-      //         transitionDuration: const Duration(milliseconds: 500),
-      //         pageBuilder: (ctx, animation1, animation2) {
-      //           return AboutPage();
-      //         });
-      //   }
       // case Experience.name:
       //   {
       //     return PageRouteBuilder(
