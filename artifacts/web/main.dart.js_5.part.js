f9bf9e0f532fcfeea6dc68bd2fad16f385fa21de
Mahesh-R-Mesta <Mahesh-R-Mesta@users.noreply.github.com>
2025-06-05 16:55:52 +0000

    Commit the artifacts.

---
+++ artifacts/web/main.dart.js_5.part.js
@@ -0,0 +1,57 @@
+((a,b,c)=>{a[b]=a[b]||{}
+a[b][c]=a[b][c]||[]
+a[b][c].push({p:"main.dart.js_5",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var J,A,D,E,F,C={
+aGw(d){return new C.JO(d,null)},
+JO:function JO(d,e){this.c=d
+this.a=e},
+a69:function a69(d,e){this.a=d
+this.b=e},
+a66:function a66(d){this.a=d},
+a68:function a68(d,e){this.a=d
+this.b=e},
+a67:function a67(d){this.a=d}},B
+J=c[1]
+A=c[0]
+D=c[2]
+E=c[5]
+F=c[13]
+C=a.updateHolder(c[6],C)
+B=c[12]
+C.JO.prototype={
+L(d){var x,w,v,u=null,t=A.b([B.Og,B.Oe],y.d),s=E.as4(A.IK(u,u,A.jq(F.mB,u,u,u),u,u,this.c,u,u,u)),r=y.l,q=J.J_(2,r)
+for(x=y.e,w=0;w<2;++w){v=new A.b3(20,20)
+q[w]=new A.p3(new A.m3(new C.a68(t,w),new C.a69(t,w),u,x),D.am,0,A.aj(4294441468),u,u,u,new A.ci(new A.cM(v,v,v,v),new A.c7(D.cf,1.4,D.R,-1)),!0,D.y,D.T,u)}r=A.Y(q,r)
+return A.a9C(s,new A.cg(D.hZ,A.f3(r,D.ae,D.F,D.Q,10),u))}}
+var z=a.updateTypes([])
+C.a69.prototype={
+$2(d,e){return new E.jM(this.a[this.b],new C.a66(e),null)},
+$S:541}
+C.a66.prototype={
+$0(){return this.a.$0()},
+$S:0}
+C.a68.prototype={
+$2(d,e){var x=null
+return A.kK(!1,!0,new A.zL(this.a[this.b],x),x,!0,x,x,x,x,x,x,x,x,new C.a67(e),x,x,x,x,x,x,x)},
+$S:89}
+C.a67.prototype={
+$0(){return this.a.$0()},
+$S:0};(function inheritance(){var x=a.inherit,w=a.inheritMany
+x(C.JO,A.aC)
+w(A.m0,[C.a69,C.a68])
+w(A.m_,[C.a66,C.a67])})()
+A.vr(b.typeUniverse,JSON.parse('{"JO":{"aC":[],"h":[]}}'))
+var y={e:A.W("m3<0&>"),g:A.W("l<jJ>"),d:A.W("l<jL>"),h:A.W("l<i>"),l:A.W("h")};(function constants(){var x=a.makeConstList
+B.J6=A.b(x(["Generates code and create files and folder inside project","Integrated Gemini API"]),y.h)
+B.Ro=new A.fe("Gemini",null)
+B.Jn=A.b(x([D.e7,B.Ro]),A.W("l<fe>"))
+B.HR=new A.iA("assets/svg/medium.svg","Medium","https://medium.com/@maheshmesta/how-i-built-a-dart-cli-tool-that-generates-flutter-react-boilerplate-with-ai-9d75ad105cf3")
+B.HU=new A.iA("assets/svg/github.svg","Github","https://github.com/Mahesh-R-Mesta/pro_pilot_cli")
+B.IV=A.b(x([B.HR,B.HU]),A.W("l<iA>"))
+B.JQ=A.b(x([D.x1]),y.g)
+B.Oe=new A.jL("Pro-Pilot CLI","A Dart CLI tool to generate boilerplate code for Flutter/React project",B.J6,"",B.Jn,B.IV,!0,B.JQ,null)
+B.IE=A.b(x(["Developed a mobile app to manage the refurbishment process of electronic accessories","Planned and developed the workflow with team","Implemented app tour and multi-language localization"]),y.h)
+B.IX=A.b(x([D.fj,D.x2]),y.g)
+B.Og=new A.jL("2ndSmart","Managing the process of refurbishing electronics accessories",B.IE,"Nov 2021 - Nov 2022",D.n6,D.n3,null,B.IX,null)})()};
+((a,b)=>{a[b]=a.current
+a.eventLog.push({p:"main.dart.js_5",e:"endPart",h:b})})($__dart_deferred_initializers__,"JSts1yGo7NkXUHToPt4pB3rf6nk=");
---
+++ artifacts/web/main.dart.js_8.part.js
@@ -0,0 +1,50 @@
+((a,b,c)=>{a[b]=a[b]||{}
+a[b][c]=a[b][c]||[]
+a[b][c].push({p:"main.dart.js_8",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A,C,E,F,G,B={
+aDg(d){return new B.o6(d,null)},
+o6:function o6(d,e){this.c=d
+this.a=e},
+Y1:function Y1(){},
+Y2:function Y2(d){this.a=d}},D
+A=c[0]
+C=c[2]
+E=c[7]
+F=c[8]
+G=c[11]
+B=a.updateHolder(c[3],B)
+D=c[10]
+B.o6.prototype={
+L(d){var x,w,v,u,t=null,s="confetti",r=A.bI(d,t,y.h).w.a,q=A.Fi(s,"")
+A.kd(s)
+A.kd(s)
+x=E.asA(D.mf)
+x.Ry()
+A.kd(s)
+w=r.a/1.5
+v=r.b/1.5
+u=this.c
+return A.li(C.bG,A.b([new A.fj(C.db,t,t,new A.hL(q,new B.Y1(),t),t),new A.fj(C.kv,t,t,E.asB(2.356194490192345,F.l3,D.n5,x,0.1,40,!0),t),A.zJ(t,A.IK(t,t,A.jq(C.GR,t,t,40),t,t,new B.Y2(d),t,t,t),t,t,t,20,20,t),A.ku(A.eV(A.atj(new A.cg(C.i_,A.a1C(u,v,w),t),u),v,w),t,t)],y.e),C.bO)}}
+var z=a.updateTypes(["m2(Q)"])
+B.Y1.prototype={
+$1(d){var x,w="confetti"
+A.kd(w)
+A.kd(w)
+x=E.asA(D.mf)
+x.Ry()
+A.kd(w)
+return E.asB(0.7853981633974483,F.l3,D.n5,x,0.1,40,!0)},
+$S:z+0}
+B.Y2.prototype={
+$0(){A.ti(this.a,!1).EC(null)
+return null},
+$S:0};(function inheritance(){var x=a.inherit
+x(B.o6,A.aC)
+x(B.Y1,A.hJ)
+x(B.Y2,A.m_)})()
+A.vr(b.typeUniverse,JSON.parse('{"o6":{"aC":[],"h":[]}}'))
+var y={e:A.W("l<h>"),h:A.W("fB")};(function constants(){var x=a.makeConstList
+D.mf=new A.aK(5e6)
+D.n5=A.b(x([C.f8,C.b_,G.Mg,C.cf,G.Mf]),A.W("l<p>"))})()};
+((a,b)=>{a[b]=a.current
+a.eventLog.push({p:"main.dart.js_8",e:"endPart",h:b})})($__dart_deferred_initializers__,"4ZtxUa+hhRPslWzBm3ofMddXv0I=");
